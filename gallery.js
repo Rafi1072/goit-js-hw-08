20348346f06a0dce4e9e4e3038364996af9463fd
Rafi1072 <rafi110-96@o2.pl>
2024-09-25 15:23:15 +0200

    changes

--- gallery.js
+++ gallery.js
@@ -63,3 +63,13 @@ const images = [
     description: "Lighthouse Coast Sea",
   },
 ];
+
+const instance = basicLightbox.create(
+  `
+	<h1>Not closable</h1>
+	<p>It's not possible to close this lightbox with a click.</p>
+`,
+  {
+    closable: false,
+  }
+);
