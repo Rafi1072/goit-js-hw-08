b56fabf84ef808182d4daf31d8435f8fa0a81d51
Rafi1072 <rafi110-96@o2.pl>
2024-09-25 18:02:26 +0200

    rendering

--- gallery.js
+++ gallery.js
@@ -64,88 +64,31 @@ const images = [
   },
 ];
 
-class Gallery {
-  #currentImageIndex;
-  #images;
-  #galleryRoot;
-  #modal;
+const el = (tag, props) => Object.assign(document.createElement(tag), props);
 
-  constructor({ images, galleryRoot, modalRoot }) {
-    this.#images = images;
-    this.#currentImageIndex = null;
+const createImages = ({ smallImage, largeImage, desc }) => {
+  const ulList = el("ul", { className: "gallery" });
 
-    this.#createGallery(galleryRoot);
-    this.#modal = this.#createModal(modalRoot);
-    this.#mountModal(galleryRoot);
-  }
+  const list = el("li", { className: "gallery-item" });
+  const aLink = el("a", { className: "gallery-link", href: smallImage });
+  const img = el("img", {
+    className: "gallery-image",
+    src: smallImage,
+    dataSource: largeImage,
+    alt: desc,
+  });
 
-  #getCurrentImage() {
-    if (this.#currentImageIndex === null) return null;
-    return this.#images[this.#currentImageIndex] ?? null;
-  }
+  ulList.append(list, aLink, img);
 
-  #createGallery(galleryRoot) {
-    const imgs = this.#images.map(this.#createImage);
+  return ulList;
+};
 
-    galleryRoot.append(...imgs);
-  }
+const renderImages = (images) => {
+  const imagesCards = images.map(createImages).map((card) => {
+    const li = el("li");
+    li.append(card);
+    return li;
+  });
 
-  #createImage({ description, preview, original }, id) {
-    const container = document.createElement("article");
-    container.id = id;
-
-    const img = document.createElement("img");
-    img.src = preview;
-    img.alt = description;
-
-    const p = document.createElement("p");
-    p.textContent = description;
-
-    container.appendChild(img);
-    container.appendChild(p);
-    return container;
-  }
-
-  #createModal(modalRoot) {
-    const onShow = this.#renderModal.bind(this);
-    return basicLightbox.create(modalRoot, { onShow });
-  }
-
-  #renderModal(lightbox) {
-    const currentImage = this.#getCurrentImage();
-    if (currentImage === null) return lightbox.close();
-
-    const modal = lightbox.element();
-
-    const img = modal.querySelector("img");
-    img.alt = currentImage.description;
-    img.src = currentImage.original;
-
-    modal.querySelector("p").textContent = currentImage.description;
-    modal.querySelector("button").onclick = lightbox.close;
-  }
-
-  #mountModal(galleryRoot) {
-    if (!this.#modal) return;
-
-    galleryRoot.addEventListener("click", (event) => {
-      if (
-        event.target.tagName === "ARTICLE" ||
-        event.target.parentNode.tagName === "ARTICLE"
-      ) {
-        this.#currentImageIndex = event.target.id || event.target.parentNode.id;
-        this.#modal.show();
-      }
-    });
-
-    document.addEventListener("keydown", () => {
-      this.#modal.close();
-    });
-  }
-}
-
-const gallery = new Gallery({
-  images,
-  galleryRoot: document.querySelector("ul.gallery"),
-  modalRoot: document.querySelector("template#modal"),
-});
+  return imagesCards;
+};
